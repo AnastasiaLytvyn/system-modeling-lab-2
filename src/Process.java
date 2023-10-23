dd3d33e658febf8ba3b7ead665b789cd6d759200
Anastasiia Lytvyn <lytvynana@gmail.com>
2023-10-23 08:15:40 +0300

    add 5 task

--- src/Process.java
+++ src/Process.java
@@ -1,21 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Process extends Element {
     private int queue, maxqueue, failure;
     private double meanQueue;
+    private final List<Integer> channelsStates = new ArrayList<>();
+    private final List<Double> channelsTnext = new ArrayList<>();
 
     public Process(double delay) {
         super(delay);
         setTnext(Double.MAX_VALUE);
-
         queue = 0;
         maxqueue = Integer.MAX_VALUE;
         meanQueue = 0.0;
     }
 
+    public Process(double delay, int channels) {
+        super(delay);
+        super.setTnext(Double.MAX_VALUE);
+        queue = 0;
+        maxqueue = Integer.MAX_VALUE;
+        meanQueue = 0.0;
+        for (int i = 0; i < channels; i++) {
+            channelsStates.add(0);
+            channelsTnext.add(Double.MAX_VALUE);
+        }
+    }
+
     @Override
     public void inAct() {
-        if (super.getState() == 0) {
+
+        if (channelsStates.size() != 0) {
+            int currentChannel = channelsStates.indexOf(0);
+            if (currentChannel != -1) {
+                channelsStates.set(currentChannel, 1);
+                channelsTnext.set(currentChannel, super.getTcurr() + super.getDelay());
+                super.setTnext(Collections.min(channelsTnext));
+            } else {
+                if (getQueue() < getMaxqueue()) {
+                    setQueue(getQueue() + 1);
+                } else {
+                    failure++;
+                }
+            }
+        } else if (super.getState() == 0) {
             super.setState(1);
             super.setTnext(super.getTcurr() + super.getDelay());
+
         } else {
             if (getQueue() < getMaxqueue()) {
                 setQueue(getQueue() + 1);
@@ -28,14 +60,29 @@ public class Process extends Element {
     @Override
     public void outAct() {
         super.outAct();
-        super.setTnext(Double.MAX_VALUE);
-        super.setState(0);
 
-        if (getQueue() > 0) {
-            setQueue(getQueue() - 1);
-            super.setState(1);
-            super.setTotalTime(super.getTotalTime()+super.getDelay());
-            super.setTnext(super.getTcurr() + super.getDelay());
+        if (channelsStates.size() != 0) {
+            int currentChannel = channelsTnext.indexOf(super.getTcurr());
+            channelsStates.set(currentChannel, 0);
+            channelsTnext.set(currentChannel, Double.MAX_VALUE);
+            super.setTnext(Collections.min(channelsTnext));
+            super.setTotalTime(super.getTotalTime() + super.getDelay());
+            if (getQueue() > 0) {
+                setQueue(getQueue() - 1);
+                channelsStates.set(currentChannel, 1);
+                channelsTnext.set(currentChannel, super.getTcurr() + super.getDelay());
+                super.setTnext(Collections.min(channelsTnext));
+            }
+        } else {
+            super.setTnext(Double.MAX_VALUE);
+            super.setState(0);
+
+            super.setTotalTime(super.getTotalTime() + super.getDelay());
+            if (getQueue() > 0) {
+                setQueue(getQueue() - 1);
+                super.setState(1);
+                super.setTnext(super.getTcurr() + super.getDelay());
+            }
         }
 
         if (getNextElements() != null) {
